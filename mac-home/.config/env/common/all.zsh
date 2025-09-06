e269a63a377ec9af6ea5c6ba4b41d431848acd7a
lu haorui <airhenry@gmail.com>
2025-09-07 00:52:17 +0800

    feat: introduce lazy-loading functionality for Conda, direnv, and Homebrew in zsh configuration, and update recompile script to clean compiled caches more efficiently

--- mac-home/.config/env/common/all.zsh
+++ mac-home/.config/env/common/all.zsh
@@ -2,4 +2,4 @@ source ~/.config/env/common/options.zsh
 source ~/.config/env/common/paths.zsh
 source ~/.config/env/common/aliases.zsh
 source ~/.config/env/common/proxy.zsh
-source ~/.config/env/common/lazyload.zsh
+source ~/.config/env/common/lazy.zsh
