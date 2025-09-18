0670b295d135ed064eb884d4021dcc2a290f84e1
lu haorui <airhenry@gmail.com>
2025-09-18 23:30:01 +0800

    【新增 Conda 镜像模板及更新 V2Ray 节点】 - 新增 `mac-home/.condarc`、`mac-home/.condarc.tuna`、`mac-home/.condarc.ustc` 模板，统一配置镜像与频道以匹配 USTC/清华源。 - 在 `mac-home/.config/env/common/vars.zsh` 预置清华镜像注释段，方便后续切换 Homebrew 拉源站点。 - 将 `mac-home/.config/v2ray/v2ray_config.json` 中的节点域名更新为最新地址，确保连接可用。

--- mac-home/.config/env/common/vars.zsh
+++ mac-home/.config/env/common/vars.zsh
@@ -29,6 +29,12 @@ export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#93a1a1'
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
 
+
+# export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+# export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+# export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+# export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
